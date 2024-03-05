a0f851369843229a7a30e29ea1eaf061f83d719a
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2024-03-04 17:54:27 -0800

    fix: tooltips wouldn't disappear

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
 import { BsPlayFill } from "react-icons/bs";
 
 type TooltipProps = {
@@ -12,23 +12,22 @@ function Tooltip({
     waitTime = 0,
 }: PropsWithChildren<TooltipProps>) {
     const [visible, setVisible] = useState<boolean>(false);
+    const [mouseEntered, setMouseEnterd] = useState<boolean>(false);
 
-    const [waitTimeout, setWaitTimout] = useState<NodeJS.Timeout | null>(null);
+    useEffect(() => {
+        if (!mouseEntered) {
+            setVisible(false);
+            return;
+        }
+        let timeout = setTimeout(() => setVisible(true), waitTime);
+        return () => clearTimeout(timeout);
+    }, [mouseEntered])
 
     return (
         <div
             className="relative"
-            onMouseEnter={() => {
-                setWaitTimout(
-                    setTimeout(() => {
-                        setVisible(true);
-                    }, waitTime)
-                );
-            }}
-            onMouseLeave={() => {
-                if (waitTimeout !== null) clearTimeout(waitTimeout);
-                setVisible(false);
-            }}
+            onMouseEnter={() => setMouseEnterd(true)}
+            onMouseLeave={() => setMouseEnterd(false)}
         >
             <div
                 className={`left-1/2 -translate-x-1/2 text-center bg-blue-500 rounded-md transition-all -top-14 whitespace-nowrap absolute p-2 px-3 ${
