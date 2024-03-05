463b17b7adbb8f0ab65fbd3db7800981420d508b
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2024-03-04 18:55:41 -0800

    refactor: table fields are all optional

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,77 +1,94 @@
 import "../assets/css/table.css";
 
 type TableProps = {
-    headers: string[];
-    data: (string | number)[][];
+    headers?: string[];
+    data?: (string | number)[][];
     footer?: string[];
     config?: TableConfig;
 };
 
 export type TableConfig = {
-    headerAlignment?: ("text-left" | "text-center" | "text-right" | undefined)[];
+    headerAlignment?: (
+        | "text-left"
+        | "text-center"
+        | "text-right"
+        | undefined
+    )[];
     dataAlignment?: ("text-left" | "text-center" | "text-right" | undefined)[];
-    footerAlignment?: ("text-left" | "text-center" | "text-right" | undefined)[];
+    footerAlignment?: (
+        | "text-left"
+        | "text-center"
+        | "text-right"
+        | undefined
+    )[];
     dataFormatter?: any[];
     dataStyler?: any[];
 };
 
-function Table({headers, data, footer, config}: TableProps) {
+function Table({ headers, data, footer, config }: TableProps) {
     return (
         <div className="flex justify-center shadow-2xl shadow-black py-2 rounded-md outline outline-neutral-700">
             <table className="flex-1">
-                <thead>
-                    <tr>
-                        {headers.map((val, idx) => (
-                            <th
-                                key={idx}
-                                className={
-                                    config?.headerAlignment
-                                        ? config.headerAlignment[idx]
-                                        : "text-left"
-                                }
-                            >
-                                {val}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((row, rowIdx) => (
-                        <tr
-                            key={rowIdx}
-                            className={`${
-                                rowIdx % 2 === 0
-                                    ? "bg-neutral-700"
-                                    : "bg-neutral-800"
-                            } bg-opacity-15 py-1`}
-                        >
-                            {row.map((val, idx) => (
-                                <td
+                {headers && (
+                    <thead>
+                        <tr>
+                            {headers.map((val, idx) => (
+                                <th
                                     key={idx}
-                                    className={`font-mono ${
-                                        config?.dataAlignment
-                                            ? config.dataAlignment[idx]
-                                            : ""
-                                    } ${
-                                        config?.dataStyler
-                                            ? config.dataStyler[idx] !==
-                                              undefined
-                                                ? config.dataStyler[idx](val, rowIdx)
-                                                : ""
-                                            : ""
-                                    }`}
+                                    className={
+                                        config?.headerAlignment
+                                            ? config.headerAlignment[idx]
+                                            : "text-left"
+                                    }
                                 >
-                                    {config?.dataFormatter
-                                        ? config.dataFormatter[idx] !==
-                                          undefined
-                                            ? config.dataFormatter[idx](val)
-                                            : val
-                                        : val}
-                                </td>
+                                    {val}
+                                </th>
                             ))}
                         </tr>
-                    ))}
-                </tbody>
+                    </thead>
+                )}
+                {data && (
+                    <tbody>
+                        {data.map((row, rowIdx) => (
+                            <tr
+                                key={rowIdx}
+                                className={`${
+                                    rowIdx % 2 === 0
+                                        ? "bg-neutral-700"
+                                        : "bg-neutral-800"
+                                } bg-opacity-15 py-1`}
+                            >
+                                {row.map((val, idx) => (
+                                    <td
+                                        key={idx}
+                                        className={`font-mono ${
+                                            config?.dataAlignment
+                                                ? config.dataAlignment[idx]
+                                                : ""
+                                        } ${
+                                            config?.dataStyler
+                                                ? config.dataStyler[idx] !==
+                                                  undefined
+                                                    ? config.dataStyler[idx](
+                                                          val,
+                                                          rowIdx
+                                                      )
+                                                    : ""
+                                                : ""
+                                        }`}
+                                    >
+                                        {config?.dataFormatter
+                                            ? config.dataFormatter[idx] !==
+                                              undefined
+                                                ? config.dataFormatter[idx](val)
+                                                : val
+                                            : val}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                )}
                 {footer && (
                     <tfoot>
                         <tr>
@@ -95,4 +112,4 @@ function Table({headers, data, footer, config}: TableProps) {
     );
 }
 
-export default Table
+export default Table;
