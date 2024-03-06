41cdffa80bd23f75c8a94b5354a388fa750c5933
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2024-03-06 14:38:54 -0800

    misc: reworded error page

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function ErrorPage() {
     return (
         <div className="flex flex-col justify-center items-center w-full h-full">
-            <h1 className="mb-10 text-[100px]">There has been an error!</h1>
+            <h1 className="mb-10 text-[50px]">Unable to load page.</h1>
             <Link
                 to="/"
                 className="hover:brightness-110 bg-red-500 p-3 rounded-md text-xl hover:scale-105 transition-all"
