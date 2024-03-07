cfb109be7470ffbf583c8cbbb259b64d58f02df3
Jalen Archer <118948506+jnarcher@users.noreply.github.com>
2024-03-06 17:56:35 -0800

    misc: re-organized layout

--- src/views/InGame.tsx
+++ src/views/InGame.tsx
@@ -1,19 +1,28 @@
 import BlindLevelCountdown from "../components/BlindLevelCountdown";
 import InGameInfo from "../components/InGameInfo";
 import PayoutDisplay from "../components/PayoutDisplay";
+import PlayerInfoCard from "../components/PlayerInfoCard";
+import { sequentialArray } from "../helpers/helpers";
+import { useTournament } from "../providers/TournamentProvider";
 
 function InGame() {
+    const { state } = useTournament();
 
     return (
         <div className="flex gap-5 w-full h-full">
             <div className="flex flex-col justify-between gap-5 w-3/4 h-full">
-                <div className="relative bg-neutral-900 rounded-md w-full min-h-10 overflow-hidden">
-               </div>
-                <div className="flex-grow overflow-hidden">
-                    <BlindLevelCountdown />
-                </div>
                 <div className="w-full min-h-10 transition-all overflow-hidden">
                     <InGameInfo />
+                </div>
+                <div className="overflow-hidden">
+                    <BlindLevelCountdown />
+                </div>
+                <div className="relative flex-grow rounded-md w-full overflow-hidden">
+                    <div className="flex gap-5 h-full overflow-x-scroll">
+                        {sequentialArray(state.playerCount).map((val) => (
+                            <PlayerInfoCard key={val} name={(1 + val).toString()} eliminated/>
+                        ))}
+                    </div>
                 </div>
             </div>
             <div className="w-1/4 h-full overflow-hidden">
